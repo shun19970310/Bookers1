d37adb381e5bd8b021ece7dcac09bc731f2331da
EC2 Default User <ec2-user@ip-172-31-84-67.ec2.internal>
2022-03-13 12:57:19 +0000

    [Add]編集画面

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,9 @@ class BooksController < ApplicationController
     @book = Book.all
   end
   def create
-    book = Book.all(list_params)
+    book = Book.new(book_params)
     book.save
-    redirect_to '/show'
+    redirect_to book_path(book.id)
   end
   def show
     @book = Book.find(params[:id])
@@ -19,10 +19,16 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    redirect_to edit_book_path(book.id)
+  end
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
   private
   # ストロングパラメータ
-  def list_params
+  def book_params
     params.require(:book).permit(:title, :body)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'top' => 'books#top'
   get 'lists/:id' => 'lists#show'
+  get 'lists/:id' => 'lists#show', as: 'list'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_list'
+
 end
